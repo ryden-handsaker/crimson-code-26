ce41a68020e0afb9664dc285261c536ef28607a9
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2026-02-21 14:58:31 -0800

    Feat: Machine changed to use input belts instead of output belts

--- scenes/game/data/Machine.cs
+++ scenes/game/data/Machine.cs
@@ -2,11 +2,20 @@ using System;
 using System.Collections.Generic;
 using CrimsonCode26.scenes.game.data;
 
-namespace CrimsonCode26.scenes.game.blocks;
+namespace CrimsonCode26.scenes.game.data;
 
 public abstract class Machine
 {
-    private readonly Dictionary<string, Belt> _outputs = new();
+    public string Name { get; }
+    public string Id { get; }
+    
+    private readonly Dictionary<string, Machine> _outputs = new();
+    private Belt _belt;
+    
+    public abstract void Run();
+        // await input belt
+        // Do processing
+        // await Outputs.get("Success").receive(file);
 
     public bool AddConnection(string name, Belt belt) =>  belt == null ? throw new ArgumentNullException(nameof(belt)) : _outputs.TryAdd(name, belt);
 }
