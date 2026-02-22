c474548983dfd80adf3c3d0e050170124daf960c
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2026-02-21 16:15:06 -0800

    Feat: Initialize added to Machine

--- scenes/game/data/Machine.cs
+++ scenes/game/data/Machine.cs
@@ -1,25 +1,34 @@
 using System;
-using System.Collections.Generic; 
 using CrimsonCode26.scenes.game.data;
+using CrimsonCode26.scenes.game.data.machines;
+using Godot.Collections;
 
 namespace CrimsonCode26.scenes.game.data;
 
 public abstract class Machine
 {
-    public string Name { get; }
-    public string Id { get; }
+    public string Name { get; protected set; }
+    public Guid Id { get; protected set; }
     
     public File ProcessFile { get; protected set; }
     
-    protected readonly Dictionary<string, Machine> _outputs = new();
-    protected Belt _belt;
+    protected readonly System.Collections.Generic.Dictionary<string, Machine> Outputs = new();
+    protected Belt Belt;
+
+    protected void Initialize(string name)
+    {
+        Name = name;
+        Id = Guid.NewGuid();
+        ProcessFile = null;
+        Belt = new Belt(this);
+    }
     
     public abstract void Process(File file);
         // await input belt
         // Do processing
         // await Outputs.get("Success").receive(file);
 
-    public bool AddConnection(string name, Machine machine) =>  machine == null ? throw new ArgumentNullException(nameof(machine)) : _outputs.TryAdd(name, machine);
+    public bool AddConnection(string name, Machine machine) =>  machine == null ? throw new ArgumentNullException(nameof(machine)) : Outputs.TryAdd(name, machine);
 
-    public bool Enqueue(File file) => _belt.Enqueue(file);
+    public bool Enqueue(File file) => Belt.Enqueue(file);
 }
