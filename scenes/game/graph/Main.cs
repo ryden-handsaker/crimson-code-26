3005b675d4cd70339ab5f1f0419279b42571f7a7
guppy <guppyadrian@gmail.com>
2026-02-22 07:45:33 -0800

    feat: delete nodes

--- scenes/game/graph/Main.cs
+++ scenes/game/graph/Main.cs
@@ -5,14 +5,11 @@ using System.Linq;
 
 namespace CrimsonCode26.scenes.game.graph;
 
-public partial class Main : HBoxContainer
+public partial class Main : Control
 {
 
 	[Export]
 	private PackedScene _machineScene;
-	
-	[Export]
-	private MachineResource _testResource;
 	
 	private GraphEdit _graphEdit;
 	private VBoxContainer _buttons;
@@ -63,12 +60,32 @@ public partial class Main : HBoxContainer
 
 	private void OnDisconnectRequest(StringName fromNode, int fromPort, StringName toNode, int toPort)
 	{
-		var outputNode = _graphEdit.GetNode<MachineTemplate>(new NodePath(toNode));
 		var inputNode = _graphEdit.GetNode<MachineTemplate>(new NodePath(fromNode));
 		
 		_graphEdit.DisconnectNode(fromNode, fromPort, toNode, toPort);
 
 		inputNode.SetOutputConnection(Guid.Empty, fromPort);
+	}
+
+	private void OnDeleteNodesRequest(StringName[] nodes)
+	{
+		foreach (var item in nodes)
+		{
+			string nodeName = item.ToString();
+
+			// Remove all connections w/ this node before deleting it
+			foreach (var c in _graphEdit.GetConnectionList()
+				.Where(c => (string)c["from_node"] == nodeName || (string)c["to_node"] == nodeName)
+				.ToList())
+			{
+				_graphEdit.DisconnectNode(
+					(string)c["from_node"], (int)(long)c["from_port"],
+					(string)c["to_node"],   (int)(long)c["to_port"]);
+			}
+
+			var node = _graphEdit.GetNode<GraphNode>(nodeName);
+			node.QueueFree();
+		}
 	}
 
 	private void AddMachine(MachineResource resource)
@@ -78,6 +95,7 @@ public partial class Main : HBoxContainer
 		machineNode.Initialize(resource);
 		
 		_graphEdit.AddChild(machineNode);
+		machineNode.PositionOffset = new Vector2(100, 100);
 	}
 
 	private void OnButtonButtonDown()
