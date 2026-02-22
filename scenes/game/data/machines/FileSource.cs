7d26f49d13c941f76ff1d5b1b8bec53ad142d26f
GoldenGuy1000 <GoldenGuy1000Contact@gmail.com>
2026-02-21 17:47:20 -0800

    Feat: created FileSource machine

---
+++ scenes/game/data/machines/FileSource.cs
@@ -0,0 +1,57 @@
+using System.ComponentModel;
+using System.IO;
+using Godot;
+
+namespace CrimsonCode26.scenes.game.data.machines;
+
+public class FileSource : Machine
+{
+    public override void Process(File file)
+    {
+        if (ProcessFile != null) throw new InvalidAsynchronousStateException(); // eh
+        
+        ProcessFile = file;
+        Outputs["Output"].Enqueue(file);
+        ProcessFile = null;
+    }
+
+    private void FileCreated(object sender, FileSystemEventArgs e)
+    {
+        GD.Print("File changed: {0}", e.Name);
+        Enqueue(new File(e.FullPath));
+    }
+
+    public FileSource(string path)
+    {
+        Initialize("File Source");
+        Belt = new Belt(this, 100);
+
+        foreach (string file in Directory.GetFiles(path))
+        {
+            if (!Enqueue(new File(file)))
+            {
+                GD.PrintErr("Maximum file count exceeded"); // TODO: just a current limitation of the architecture
+            }
+        }
+
+        // https://learn.microsoft.com/en-us/dotnet/api/system.io.filesystemwatcher?view=net-10.0 
+        using var watcher = new FileSystemWatcher(path);
+
+        // some of these filters are definitely unnecessary
+        watcher.NotifyFilter = NotifyFilters.Attributes
+                               | NotifyFilters.CreationTime
+                               | NotifyFilters.DirectoryName
+                               | NotifyFilters.FileName
+                               | NotifyFilters.LastAccess
+                               | NotifyFilters.LastWrite
+                               | NotifyFilters.Security
+                               | NotifyFilters.Size;
+
+        // watcher.Changed += OnChanged; // maybe someday
+        watcher.Created += FileCreated;
+
+        // watcher.IncludeSubdirectories = true;
+        watcher.EnableRaisingEvents = true;
+    }
+
+}
